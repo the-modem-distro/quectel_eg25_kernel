0e3de0c37bc3e5c3aef7534c5228abfb4bf9d213
Biktor <sleepyh@gmail.com>
2021-12-11 12:12:59 +0100

    Don't abort wakeup when pm_runtime returns 1

--- drivers/usb/gadget/ci13xxx_udc.c
+++ drivers/usb/gadget/ci13xxx_udc.c
@@ -1683,10 +1683,6 @@ static int ci13xxx_wakeup(struct usb_gadget *_gadget)
 	
 	if (ret == 1) {
 		pr_info("%s: Already awake\n", __func__);
-		retry_count = 0;
-		pm_runtime_mark_last_busy(&_gadget->dev);
-		pm_runtime_put_autosuspend(&_gadget->dev);
-		return 0;
 	}
 
 	retry_count = 0;
